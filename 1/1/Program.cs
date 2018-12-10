452e639599c6e54ffd6c0f250226b66976a82400
busesorin94 <sorin-marian.buse@my.fmi.unibuc.ro>
2018-12-10 22:09:13 +0200

    Solve Day1

---
+++ 1/1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Solve();
+            }
+        }
+
+        private static void Solve()
+        {
+            string input = null;
+            int result = 0;
+            HashSet<int> freq = new HashSet<int>();
+            freq.Add(result);   //Initial value
+            string firstTwiceSet = string.Empty;
+            bool foundDuplicate = false;
+            bool firstItteration = true;
+            var allValues = File.ReadAllLines("../../input.in");
+            int index = 0;
+            while (index < allValues.Length || (!foundDuplicate && (index=0)==0))
+            {
+                input = allValues[index];
+                var number = int.Parse(input.Substring(1));
+                switch (input[0])
+                {
+                    case '-':
+                        result -= number;
+                        break;
+                    case '+':
+                        result += number;
+                        break;
+                }
+                if (!foundDuplicate && freq.Contains(result))
+                {
+                    Console.WriteLine("First duplicate = {0}", result);
+                    foundDuplicate = true;
+                }
+                freq.Add(result);
+                index++;
+                if(firstItteration && index == allValues.Length)
+                {
+                    Console.WriteLine("Result = {0}", result);
+                    firstItteration = false;
+                }
+            }
+        }
+    }
+}
